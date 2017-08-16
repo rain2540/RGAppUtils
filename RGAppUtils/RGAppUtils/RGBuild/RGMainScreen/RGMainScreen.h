2c071442309e15f5df52f797478fb8abce7a8345
Smartech <rain2540@foxmail.com>
2017-08-16 09:13:45 +0800

    feat (RGBuild) : add RGMainScreen

---
+++ RGAppUtils/RGAppUtils/RGBuild/RGMainScreen/RGMainScreen.h
@@ -0,0 +1,13 @@
+//
+//  RGMainScreen.h
+//  RGAppUtils
+//
+//  Created by RAIN on 2017/8/16.
+//  Copyright © 2017年 Smartech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RGMainScreen : UIScreen
+
+@end
