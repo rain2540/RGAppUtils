0008426f31c9ee5a571c9505be98a3ce9c3cc4bb
Smartech <rain2540@foxmail.com>
2017-08-17 09:45:11 +0800

    feat (RGUIConstant) : add variables

--- RGAppUtils/RGAppUtils/App Commons/RGUIConstant.h
+++ RGAppUtils/RGAppUtils/App Commons/RGUIConstant.h
@@ -6,8 +6,59 @@
 //  Copyright © 2017年 Smartech. All rights reserved.
 //
 
+#import <UIKit/UIKit.h>
+
 #ifndef RGUIConstant_h
 #define RGUIConstant_h
 
+/**
+ *  Status Bar 默认高度
+ */
+static const CGFloat StatusBarDefaultHeight = 20.0f;
+
+/**
+ *  Navigation Bar 默认高度
+ */
+static const CGFloat NavigationBarDefaultHeight = 44.0f;
+
+/**
+ *  Top Layout 默认高度
+ */
+static const CGFloat TopLayoutDefaultHeight = StatusBarDefaultHeight + NavigationBarDefaultHeight;
+
+/**
+ *  Tab Bar 默认高度
+ */
+static const CGFloat TabBarDefaultHeight = 49.0f;
+
+/**
+ *  Tool Bar 默认高度
+ */
+static const CGFloat ToolBarDefaultHeight = 44.0f;
+
+/**
+ *  视图默认间距
+ */
+static const CGFloat ViewDefaultInterval = 8.0f;
+
+/**
+ *  Label 默认高度
+ */
+static const CGFloat LabelDefaultHeight = 21.0f;
+
+/**
+ *  Button 默认高度
+ */
+static const CGFloat ButtonDefaultHeight = 44.0f;
+
+/**
+ *  TextField 默认高度
+ */
+static const CGFloat TextFieldDefaultHeight = 30.0f;
+
+/**
+ *  TableViewCell 默认高度
+ */
+static const CGFloat TableViewCellDefaultHeight = 44.0f;
 
 #endif /* RGUIConstant_h */
