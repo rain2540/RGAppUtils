e27d9ef26219f4bbcb83a82842145a3ee480dc0b
Smartech <rain2540@foxmail.com>
2017-08-18 16:20:41 +0800

    feat (UIAlertController) : add methods

--- RGAppUtils/RGAppUtils/Categories/UIKit/UIAlertController+RGAppUtils.h
+++ RGAppUtils/RGAppUtils/Categories/UIKit/UIAlertController+RGAppUtils.h
@@ -10,4 +10,30 @@
 
 @interface UIAlertController (RGAppUtils)
 
+/**
+ 在视图中添加 Alert
+
+ @param title Alert 标题
+ @param message Alert 内容
+ @param viewController 展示 Alert 的视图
+ @param actions 完成的 actions
+ */
++ (void)rg_showAlertWithTitle:(NSString *)title
+                      message:(NSString *)message
+             inViewController:(UIViewController *)viewController
+                      actions:(NSArray *)actions;
+
+/**
+ 在视图中添加 ActionSheet
+
+ @param title ActionSheet 标题
+ @param message ActionSheet 内容
+ @param viewController 展示 ActionSheet 的视图
+ @param actions 完成的 actions
+ */
++ (void)rg_showActionSheetWithTitle:(NSString *)title
+                            message:(NSString *)message
+                   inViewController:(UIViewController *)viewController
+                            actions:(NSArray *)actions;
+
 @end
--- RGAppUtils/RGAppUtils/Categories/UIKit/UIAlertController+RGAppUtils.m
+++ RGAppUtils/RGAppUtils/Categories/UIKit/UIAlertController+RGAppUtils.m
@@ -10,4 +10,45 @@
 
 @implementation UIAlertController (RGAppUtils)
 
++ (void)rg_showAlertWithTitle:(NSString *)title
+                      message:(NSString *)message
+             inViewController:(UIViewController *)viewController
+                      actions:(NSArray *)actions
+{
+    [[self class] rg_showWithTitle:title
+                           message:message
+                    preferredStyle:UIAlertControllerStyleAlert
+                  inViewController:viewController
+                           actions:actions];
+}
+
++ (void)rg_showActionSheetWithTitle:(NSString *)title
+                            message:(NSString *)message
+                   inViewController:(UIViewController *)viewController
+                            actions:(NSArray *)actions
+{
+    [[self class] rg_showWithTitle:title
+                           message:message
+                    preferredStyle:UIAlertControllerStyleActionSheet
+                  inViewController:viewController
+                           actions:actions];
+}
+
+#pragma mark Private Method
++ (void)rg_showWithTitle:(NSString *)title
+                 message:(NSString *)message
+          preferredStyle:(UIAlertControllerStyle)style
+        inViewController:(UIViewController *)viewController
+                 actions:(NSArray *)actions
+{
+    UIAlertController *alertController =
+    [UIAlertController alertControllerWithTitle:title
+                                        message:message
+                                 preferredStyle:style];
+    for (UIAlertAction *action in actions) {
+        [alertController addAction:action];
+    }
+    [viewController presentViewController:alertController animated:YES completion:nil];
+}
+
 @end
