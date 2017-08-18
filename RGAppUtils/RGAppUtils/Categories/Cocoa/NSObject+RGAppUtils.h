6e2b5e7ab77c3bfc46b5605732aac3339e7c1137
Smartech <rain2540@foxmail.com>
2017-08-18 15:24:29 +0800

    feat (NSObject) : add methods

--- RGAppUtils/RGAppUtils/Categories/Cocoa/NSObject+RGAppUtils.h
+++ RGAppUtils/RGAppUtils/Categories/Cocoa/NSObject+RGAppUtils.h
@@ -10,4 +10,18 @@
 
 @interface NSObject (RGAppUtils)
 
+/**
+ 获取去掉 nil 或 NSNull 后的字符串
+
+ @return 去掉 nil 或 NSNull 后的字符串
+ */
+- (nonnull NSString *)rg_withoutNull;
+
+/**
+ 转换为 JSON String
+
+ @return 转换出的 JSON String
+ */
+- (nullable NSString *)rg_objectToJSONString;
+
 @end
--- RGAppUtils/RGAppUtils/Categories/Cocoa/NSObject+RGAppUtils.m
+++ RGAppUtils/RGAppUtils/Categories/Cocoa/NSObject+RGAppUtils.m
@@ -10,4 +10,24 @@
 
 @implementation NSObject (RGAppUtils)
 
+- (nonnull NSString *)rg_withoutNull {
+    if (self == nil || [self isKindOfClass:[NSNull class]]) {
+        return @"";
+    } else if ([self isKindOfClass:[NSNumber class]]) {
+        return [NSString stringWithFormat:@"%@", self];
+    } else {
+        return [NSString stringWithFormat:@"%@", self];
+    }
+}
+
+- (NSString *)rg_objectToJSONString {
+    NSError *error;
+    NSData *data = [NSJSONSerialization dataWithJSONObject:self
+                                                   options:NSJSONWritingPrettyPrinted
+                                                     error:&error];
+    NSLog(@"RGAppUtils NSObject to JSON string Error:\n%@", error);
+
+    return [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+}
+
 @end
