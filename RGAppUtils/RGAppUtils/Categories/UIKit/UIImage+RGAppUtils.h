e6095da77a367fb9f392463577d47158fbcca16a
Smartech <rain2540@foxmail.com>
2017-08-21 15:56:37 +0800

    feat (UIImage) : add methods

--- RGAppUtils/RGAppUtils/Categories/UIKit/UIImage+RGAppUtils.h
+++ RGAppUtils/RGAppUtils/Categories/UIKit/UIImage+RGAppUtils.h
@@ -10,4 +10,26 @@
 
 @interface UIImage (RGAppUtils)
 
+/**
+ 获取 Image 对应的 Data
+
+ @return Image 对应的 Data
+ */
+- (NSData *)rg_data;
+
+/**
+ 获取 Image 对应的 Base64 String
+
+ @return Image 对应的 Base64 String
+ */
+- (NSString *)rg_base64String;
+
+/**
+ 将图片转换到新的尺寸, 并返回新尺寸的图片对象
+
+ @param newSize 图片新尺寸
+ @return 新尺寸的图片对象
+ */
+- (UIImage *)rg_transToNewSize:(CGSize)newSize;
+
 @end
--- RGAppUtils/RGAppUtils/Categories/UIKit/UIImage+RGAppUtils.m
+++ RGAppUtils/RGAppUtils/Categories/UIKit/UIImage+RGAppUtils.m
@@ -10,4 +10,40 @@
 
 @implementation UIImage (RGAppUtils)
 
+- (NSData *)rg_data {
+    NSData *jpegData = UIImageJPEGRepresentation(self, 1.0);
+    if (jpegData != nil) {
+        return jpegData;
+    } else {
+        NSData *pngData = UIImagePNGRepresentation(self);
+        if (pngData != nil) {
+            return pngData;
+        } else {
+            return nil;
+        }
+    }
+}
+
+- (NSString *)rg_base64String {
+    NSData *data = [self rg_data];
+    if (data == nil) {
+        return nil;
+    } else {
+        return [data base64EncodedStringWithOptions:NSDataBase64Encoding64CharacterLineLength];
+    }
+}
+
+- (UIImage *)rg_transToNewSize:(CGSize)newSize {
+    UIGraphicsBeginImageContext(newSize);
+    [self drawInRect:CGRectMake(0.0, 0.0, newSize.width, newSize.height)];
+    UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
+    if (newImage == nil) {
+        UIGraphicsEndImageContext();
+        return nil;
+    } else {
+        UIGraphicsEndImageContext();
+        return newImage;
+    }
+}
+
 @end
