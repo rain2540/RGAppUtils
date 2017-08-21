9666e83ee28881f6cd82957c0ab95587d985b8dd
Smartech <rain2540@foxmail.com>
2017-08-21 15:45:46 +0800

    feat (UIViewController) : add methods

--- RGAppUtils/RGAppUtils/Categories/UIKit/UIViewController+RGAppUtils.h
+++ RGAppUtils/RGAppUtils/Categories/UIKit/UIViewController+RGAppUtils.h
@@ -10,4 +10,11 @@
 
 @interface UIViewController (RGAppUtils)
 
+/**
+ 获取位于最上层的视图
+
+ @return 位于最上层的视图
+ */
+- (UIViewController *)rg_topViewController;
+
 @end
--- RGAppUtils/RGAppUtils/Categories/UIKit/UIViewController+RGAppUtils.m
+++ RGAppUtils/RGAppUtils/Categories/UIKit/UIViewController+RGAppUtils.m
@@ -10,4 +10,19 @@
 
 @implementation UIViewController (RGAppUtils)
 
+- (UIViewController *)rg_topViewController {
+    if ([self isKindOfClass:[UITabBarController class]]) {
+        UITabBarController *tabBarController = (UITabBarController *)self;
+        return [tabBarController.selectedViewController rg_topViewController];
+    } else if ([self isKindOfClass:[UINavigationController class]]) {
+        UINavigationController *navigationController = (UINavigationController *)self;
+        return [navigationController.visibleViewController rg_topViewController];
+    } else if (self.presentedViewController) {
+        UIViewController *presentedViewController = self.presentedViewController;
+        return [presentedViewController rg_topViewController];
+    } else {
+        return self;
+    }
+}
+
 @end
