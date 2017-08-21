8fb15287649342e16112a1f001d68b3974779c41
Smartech <rain2540@foxmail.com>
2017-08-21 11:23:18 +0800

    feat (UIColor) : add methods

--- RGAppUtils/RGAppUtils/Categories/UIKit/UIColor+RGAppUtils.h
+++ RGAppUtils/RGAppUtils/Categories/UIKit/UIColor+RGAppUtils.h
@@ -10,4 +10,52 @@
 
 @interface UIColor (RGAppUtils)
 
+/**
+ 通过指定的不透明度和 RGB 分量值, 初始化并返回一个颜色对象
+
+ @param red 红色分量的值 (0 ~ 255)
+ @param green 绿色分量的值 (0 ~ 255)
+ @param blue 蓝色分量的值 (0 ~ 255)
+ @param alpha 不透明度的值 (0 ~ 1)
+ @return 被初始化的颜色对象
+ */
+- (UIColor *)rg_initWithRed:(CGFloat)red
+                      green:(CGFloat)green
+                       blue:(CGFloat)blue
+                      alpha:(CGFloat)alpha;
+
+/**
+ 通过指定的不透明度和 RGB 分量值, 初始化并返回一个颜色对象
+
+ @param red 红色分量的值 (0 ~ 255)
+ @param green 绿色分量的值 (0 ~ 255)
+ @param blue 蓝色分量的值 (0 ~ 255)
+ @param alpha 不透明度的值 (0 ~ 1)
+ @return 被初始化的颜色对象
+ */
++ (UIColor *)rg_colorWithRed:(CGFloat)red
+                       green:(CGFloat)green
+                        blue:(CGFloat)blue
+                       alpha:(CGFloat)alpha;
+
+/**
+ 通过指定的不透明度 和 一个用16进制数字表示 RGB 分量值的字符串, 初始化并返回一个颜色对象
+
+ @param hexString 以"#"或"0x"开头, 后面跟随6位(或3位)16进制数字 表示RGB分量值的字符串
+ @param alpha 不透明度 (0 ~ 1)
+ @return 被初始化的颜色对象
+ */
+- (UIColor *)rg_initWithHexString:(NSString *)hexString
+                            alpha:(CGFloat)alpha;
+
+/**
+ 通过指定的不透明度 和 一个用16进制数字表示 RGB 分量值的字符串, 初始化并返回一个颜色对象
+
+ @param hexString 以"#"或"0x"开头, 后面跟随6位(或3位)16进制数字 表示RGB分量值的字符串
+ @param alpha 不透明度 (0 ~ 1)
+ @return 被初始化的颜色对象
+ */
++ (UIColor *)rg_colorWithHexString:(NSString *)hexString
+                             alpha:(CGFloat)alpha;
+
 @end
--- RGAppUtils/RGAppUtils/Categories/UIKit/UIColor+RGAppUtils.m
+++ RGAppUtils/RGAppUtils/Categories/UIKit/UIColor+RGAppUtils.m
@@ -10,4 +10,75 @@
 
 @implementation UIColor (RGAppUtils)
 
+- (UIColor *)rg_initWithRed:(CGFloat)red
+                      green:(CGFloat)green
+                       blue:(CGFloat)blue
+                      alpha:(CGFloat)alpha
+{
+    return [self initWithRed:red / 255.0
+                       green:green / 255.0
+                        blue:blue / 255.0
+                       alpha:alpha];
+}
+
++ (UIColor *)rg_colorWithRed:(CGFloat)red
+                       green:(CGFloat)green
+                        blue:(CGFloat)blue
+                       alpha:(CGFloat)alpha
+{
+    return [[UIColor alloc] rg_initWithRed:red
+                                     green:green
+                                      blue:blue
+                                     alpha:alpha];
+}
+
+- (UIColor *)rg_initWithHexString:(NSString *)hexString
+                            alpha:(CGFloat)alpha
+{
+    CGFloat red = 0.0f, green = 0.0f, blue = 0.0f;
+    if (nil != hexString) {
+        hexString = [[hexString stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]] uppercaseString];
+        if (!([hexString hasPrefix:@"0X"] || [hexString hasPrefix:@"#"])) {
+            NSLog(@"Invalid RGB hex string, missing '#' or '0x' as prefix.");
+        } else {
+
+            if ([hexString hasPrefix:@"0X"]) {
+                hexString = [hexString substringFromIndex:2];
+            } else if ([hexString hasPrefix:@"#"]) {
+                hexString = [hexString substringFromIndex:1];
+            }
+            NSScanner *scanner = [NSScanner scannerWithString:hexString];
+            unsigned long long hexValue = 0;
+            if (![scanner scanHexLongLong:&hexValue]) {
+                NSLog(@"Scan hex error.");
+            } else {
+                switch (hexString.length) {
+                    case 3:
+                        red   = ((hexValue & 0xF00) >> 8) / 15.0f;
+                        green = ((hexValue & 0x0F0) >> 4) / 15.0f;
+                        blue  = (hexValue  & 0x00F)       / 15.0f;
+                        break;
+
+                    case 6:
+                        red   = ((hexValue & 0xFF0000) >> 16) / 255.0f;
+                        green = ((hexValue & 0x00FF00) >> 8)  / 255.0f;
+                        blue  = (hexValue  & 0x0000FF)        / 255.0f;
+                        break;
+
+                    default:
+                        NSLog(@"Invalid RGB hex string, number of characters after '#' or '0x' should be either 3 or 6.");
+                        break;
+                }
+            }
+        }
+    }
+    return [self initWithRed:red green:green blue:blue alpha:alpha];
+}
+
++ (UIColor *)rg_colorWithHexString:(NSString *)hexString
+                             alpha:(CGFloat)alpha
+{
+    return [[UIColor alloc] rg_initWithHexString:hexString alpha:alpha];
+}
+
 @end
