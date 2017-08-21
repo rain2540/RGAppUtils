691d7357b7156ab7d9c40d42e5cbc4b7118f1f2a
Smartech <rain2540@foxmail.com>
2017-08-21 10:06:34 +0800

    feat (NSUserDefaults) : add methods

--- RGAppUtils/RGAppUtils/Categories/Cocoa/NSUserDefaults+RGAppUtils.h
+++ RGAppUtils/RGAppUtils/Categories/Cocoa/NSUserDefaults+RGAppUtils.h
@@ -10,4 +10,66 @@
 
 @interface NSUserDefaults (RGAppUtils)
 
+/**
+ 检验 UserDefaults 中是否存在某个 key
+
+ @param key 待检验的 key
+ @return 检验结果的布尔值
+ */
+- (BOOL)rg_isHaveKey:(NSString *)key;
+
+/**
+ 将某个 key - Object 值存入 UserDefaults 中
+
+ @param object 将要存入的 object
+ @param key 将要存入的 key
+ */
+- (void)rg_saveObject:(id)object
+               forKey:(NSString *)key;
+
+/**
+ 将某个 key - Integer 值存入 UserDefaults 中
+
+ @param value 将要存入的 Integer 值
+ @param key 将要存入的 key
+ */
+- (void)rg_saveInteger:(NSInteger)value
+                forKey:(NSString *)key;
+
+/**
+ 将某个 key - Float 值存入 UserDefaults 中
+
+ @param value 将要存入的 Float 值
+ @param key 将要存入的 key
+ */
+- (void)rg_saveFloat:(float)value
+              forKey:(NSString *)key;
+
+/**
+ 将某个 key - Double 值存入 UserDefaults 中
+
+ @param value 将要存入的 Double 值
+ @param key 将要存入的 key
+ */
+- (void)rg_saveDouble:(double)value
+               forKey:(NSString *)key;
+
+/**
+ 将某个 key - Bool 值存入 UserDefaults 中
+
+ @param value 将要存入的 Bool 值
+ @param key 将要存入的 key
+ */
+- (void)rg_saveBool:(BOOL)value
+             forKey:(NSString *)key;
+
+/**
+ 将某个 key - URL 存入 UserDefaults 中
+
+ @param url 将要存入的 URL
+ @param key 将要存入的 key
+ */
+- (void)rg_saveURL:(NSURL *)url
+            forKey:(NSString *)key;
+
 @end
--- RGAppUtils/RGAppUtils/Categories/Cocoa/NSUserDefaults+RGAppUtils.m
+++ RGAppUtils/RGAppUtils/Categories/Cocoa/NSUserDefaults+RGAppUtils.m
@@ -10,4 +10,57 @@
 
 @implementation NSUserDefaults (RGAppUtils)
 
+- (BOOL)rg_isHaveKey:(NSString *)key {
+    NSDictionary *userDefaultsDic = self.dictionaryRepresentation;
+    NSArray *keys = userDefaultsDic.allKeys;
+    for (NSString *aKey in keys) {
+        if ([key isEqualToString:aKey]) {
+            return YES;
+        }
+    }
+    return NO;
+}
+
+- (void)rg_saveObject:(id)object
+               forKey:(NSString *)key
+{
+    [self setObject:object forKey:key];
+    [self synchronize];
+}
+
+- (void)rg_saveInteger:(NSInteger)value
+                forKey:(NSString *)key
+{
+    [self setInteger:value forKey:key];
+    [self synchronize];
+}
+
+- (void)rg_saveFloat:(float)value
+              forKey:(NSString *)key
+{
+    [self setFloat:value forKey:key];
+    [self synchronize];
+}
+
+- (void)rg_saveDouble:(double)value
+               forKey:(NSString *)key
+{
+    [self setDouble:value forKey:key];
+    [self synchronize];
+}
+
+- (void)rg_saveBool:(BOOL)value
+             forKey:(NSString *)key
+{
+    [self setBool:value forKey:key];
+    [self synchronize];
+}
+
+- (void)rg_saveURL:(NSURL *)url
+            forKey:(NSString *)key
+{
+    [self setURL:url forKey:key];
+    [self synchronize];
+}
+
 @end
