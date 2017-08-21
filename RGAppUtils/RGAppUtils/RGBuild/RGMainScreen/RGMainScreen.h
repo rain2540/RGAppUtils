e95b4047ae35a36214cd55688f220e55c77abddb
Smartech <rain2540@foxmail.com>
2017-08-21 11:07:59 +0800

    feat (RGMainScreen) : add methods

--- RGAppUtils/RGAppUtils/RGBuild/RGMainScreen/RGMainScreen.h
+++ RGAppUtils/RGAppUtils/RGBuild/RGMainScreen/RGMainScreen.h
@@ -10,4 +10,32 @@
 
 @interface RGMainScreen : UIScreen
 
+/**
+ 获取 Main Screen 的 width
+
+ @return Main Screen 的 width
+ */
++ (CGFloat)width;
+
+/**
+ 获取 Main Screen 的 height
+
+ @return Main Screen 的 height
+ */
++ (CGFloat)height;
+
+/**
+ 获取 Main Screen 的 size
+
+ @return Main Screen 的 size
+ */
++ (CGSize)size;
+
+/**
+ 获取 Main Screen 的 bounds
+
+ @return Main Screen 的 bounds
+ */
++ (CGRect)bounds;
+
 @end
--- RGAppUtils/RGAppUtils/RGBuild/RGMainScreen/RGMainScreen.m
+++ RGAppUtils/RGAppUtils/RGBuild/RGMainScreen/RGMainScreen.m
@@ -10,4 +10,20 @@
 
 @implementation RGMainScreen
 
++ (CGFloat)width {
+    return [RGMainScreen size].width;
+}
+
++ (CGFloat)height {
+    return [RGMainScreen size].height;
+}
+
++ (CGSize)size {
+    return [RGMainScreen bounds].size;
+}
+
++ (CGRect)bounds {
+    return [RGMainScreen mainScreen].bounds;
+}
+
 @end
