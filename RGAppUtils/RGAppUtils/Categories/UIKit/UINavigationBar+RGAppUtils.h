108a0d95c7cb687ed687384435ac396759c9fb25
Smartech <rain2540@foxmail.com>
2017-08-21 13:54:25 +0800

    feat (UINavigationBar) : add methods

--- RGAppUtils/RGAppUtils/Categories/UIKit/UINavigationBar+RGAppUtils.h
+++ RGAppUtils/RGAppUtils/Categories/UIKit/UINavigationBar+RGAppUtils.h
@@ -10,4 +10,25 @@
 
 @interface UINavigationBar (RGAppUtils)
 
+/**
+ 修改 Navigation Bar 的背景颜色
+
+ @param color 提供给 Navigation Bar 的背景的 tint color
+ */
++ (void)rg_barTintColor:(UIColor *)color;
+
+/**
+ 修改 Navigation item 的 tint color
+
+ @param color 提供给 Navigation item 的 tint color
+ */
++ (void)rg_tintColor:(UIColor *)color;
+
+/**
+ 修改 Navigation Bar 的标题文字颜色
+
+ @param color 提供给 Navigation Bar 标题文字的颜色
+ */
++ (void)rg_titleTextColor:(UIColor *)color;
+
 @end
--- RGAppUtils/RGAppUtils/Categories/UIKit/UINavigationBar+RGAppUtils.m
+++ RGAppUtils/RGAppUtils/Categories/UIKit/UINavigationBar+RGAppUtils.m
@@ -10,4 +10,16 @@
 
 @implementation UINavigationBar (RGAppUtils)
 
++ (void)rg_barTintColor:(UIColor *)color {
+    [UINavigationBar appearance].barTintColor = color;
+}
+
++ (void)rg_tintColor:(UIColor *)color {
+    [UINavigationBar appearance].tintColor = color;
+}
+
++ (void)rg_titleTextColor:(UIColor *)color {
+    [UINavigationBar appearance].titleTextAttributes = @{NSForegroundColorAttributeName: color};
+}
+
 @end
