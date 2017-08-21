fb7a686171e4f8cf015d4ffff7b0a2c2409ab777
Smartech <rain2540@foxmail.com>
2017-08-21 13:46:43 +0800

    feat (UIDevice) : add methods

--- RGAppUtils/RGAppUtils/Categories/UIKit/UIDevice+RGAppUtils.h
+++ RGAppUtils/RGAppUtils/Categories/UIKit/UIDevice+RGAppUtils.h
@@ -8,6 +8,77 @@
 
 #import <UIKit/UIKit.h>
 
+typedef NS_ENUM(NSInteger, DeviceType) {
+    DeviceType_Unknown,
+    //  iPhone
+    DeviceType_iPhone_2G,
+    DeviceType_iPhone_3G,
+    DeviceType_iPhone_3GS,
+    DeviceType_iPhone_4,
+    DeviceType_iPhone_4S,
+    DeviceType_iPhone_5,
+    DeviceType_iPhone_5c,
+    DeviceType_iPhone_5s,
+    DeviceType_iPhone_6_Plus,
+    DeviceType_iPhone_6,
+    DeviceType_iPhone_6s,
+    DeviceType_iPhone_6s_Plus,
+    DeviceType_iPhone_SE,
+    DeviceType_iPhone_7,
+    DeviceType_iPhone_7_Plus,
+    //  iPod Touch
+    DeviceType_iPod_Touch_1,
+    DeviceType_iPod_Touch_2,
+    DeviceType_iPod_Touch_3,
+    DeviceType_iPod_Touch_4,
+    DeviceType_iPod_Touch_5,
+    DeviceType_iPod_Touch_6,
+    //  iPad
+    DeviceType_iPad_1,
+    DeviceType_iPad_2,
+    DeviceType_iPad_Mini,
+    DeviceType_iPad_3,
+    DeviceType_iPad_4,
+    DeviceType_iPad_Air,
+    DeviceType_iPad_Air_2,
+    DeviceType_iPad_Mini_2,
+    DeviceType_iPad_Mini_3,
+    DeviceType_iPad_Mini_4,
+    DeviceType_iPad_Pro,
+    //  Apple TV
+    DeviceType_Apple_TV,
+    //  iPhone Simulator
+    DeviceType_iPhone_Simulator_32bit,
+    DeviceType_iPhone_Simulator_64bit
+};
+
 @interface UIDevice (RGAppUtils)
+
+/**
+ 打印当前设备信息
+ */
++ (void)rg_printDeviceInformations;
+
+/**
+ 判断当前设备系统版本号是否大于等于某个版本号
+
+ @param version 需要判断的版本号
+ @return 判断结果
+ */
++ (BOOL)rg_systemVersionGreaterThanOrEqualTo:(NSString *)version;
+
+/**
+ 获取设备时间
+
+ @return 描述设备时间的字符串
+ */
++ (NSString *)rg_stringOfDeviceTime;
+
+/**
+ 获取当前设备类型
+
+ @return 设备类型
+ */
++ (DeviceType)rg_deviceType;
 
 @end
--- RGAppUtils/RGAppUtils/Categories/UIKit/UIDevice+RGAppUtils.m
+++ RGAppUtils/RGAppUtils/Categories/UIKit/UIDevice+RGAppUtils.m
@@ -7,7 +7,283 @@
 //
 
 #import "UIDevice+RGAppUtils.h"
+#import <sys/utsname.h>
 
 @implementation UIDevice (RGAppUtils)
+
++ (void)rg_printDeviceInformations {
+    NSLog(@"currentDevice: %@", [UIDevice currentDevice]);
+
+    UIDevice *device = [[UIDevice alloc] init];
+    NSLog(@"device.name: %@", device.name);
+    NSLog(@"device.model:%@", device.model);
+    NSLog(@"device.localizedModel: %@", device.localizedModel);
+
+    NSLog(@"device.systemName: %@", device.systemName);
+    NSLog(@"device.systemVersion: %@", device.systemVersion);
+
+    NSLog(@"device.identifierForVendor: %@", device.identifierForVendor);
+}
+
++ (BOOL)rg_systemVersionGreaterThanOrEqualTo:(NSString *)version {
+    return [[[UIDevice currentDevice] systemVersion] compare:version options:NSNumericSearch] != NSOrderedAscending;
+}
+
++ (NSString *)rg_stringOfDeviceTime {
+    NSDate *deviceTime = [NSDate date];
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    [dateFormatter setDateFormat:@"YYYY-MM-dd-HH-mm-ss"];
+    return [dateFormatter stringFromDate:deviceTime];
+}
+
++ (DeviceType)rg_deviceType {
+    struct utsname systemInfo;
+
+    uname(&systemInfo);
+
+    NSString *platform = [NSString stringWithCString:systemInfo.machine encoding:NSASCIIStringEncoding];
+
+    if ([platform isEqualToString:@"iPhone1,1"]) {
+        NSLog(@"RGAppUtils Device Type: iPhone 2G");
+        return DeviceType_iPhone_2G;
+    }
+
+    if ([platform isEqualToString:@"iPhone1,2"]) {
+        NSLog(@"RGAppUtils Device Type: iPhone 3G");
+        return DeviceType_iPhone_3G;
+    }
+
+    if ([platform isEqualToString:@"iPhone2,1"]) {
+        NSLog(@"RGAppUtils Device Type: iPhone 3GS");
+        return DeviceType_iPhone_3GS;
+    }
+
+    if ([platform isEqualToString:@"iPhone3,1"]) {
+        NSLog(@"RGAppUtils Device Type: iPhone 4");
+        return DeviceType_iPhone_4;
+    }
+
+    if ([platform isEqualToString:@"iPhone3,2"]) {
+        NSLog(@"RGAppUtils Device Type: iPhone 4");
+        return DeviceType_iPhone_4;
+    }
+
+    if ([platform isEqualToString:@"iPhone3,3"]) {
+        NSLog(@"RGAppUtils Device Type: iPhone 4");
+        return DeviceType_iPhone_4;
+    }
+
+    if ([platform isEqualToString:@"iPhone4,1"]) {
+        NSLog(@"RGAppUtils Device Type: iPhone 4S");
+        return DeviceType_iPhone_4S;
+    }
+
+    if ([platform isEqualToString:@"iPhone5,1"]) {
+        NSLog(@"RGAppUtils Device Type: iPhone 5");
+        return DeviceType_iPhone_5;
+    }
+
+    if ([platform isEqualToString:@"iPhone5,2"]) {
+        NSLog(@"RGAppUtils Device Type: iPhone 5");
+        return DeviceType_iPhone_5;
+    }
+
+    if ([platform isEqualToString:@"iPhone5,3"]) {
+        NSLog(@"RGAppUtils Device Type: iPhone 5c");
+        return DeviceType_iPhone_5c;
+    }
+
+    if ([platform isEqualToString:@"iPhone5,4"]) {
+        NSLog(@"RGAppUtils Device Type: iPhone 5c");
+        return DeviceType_iPhone_5c;
+    }
+
+    if ([platform isEqualToString:@"iPhone6,1"]) {
+        NSLog(@"RGAppUtils Device Type: iPhone 5s");
+        return DeviceType_iPhone_5s;
+    }
+
+    if ([platform isEqualToString:@"iPhone6,2"]) {
+        NSLog(@"RGAppUtils Device Type: iPhone 5s");
+        return DeviceType_iPhone_5s;
+    }
+
+    if ([platform isEqualToString:@"iPhone7,1"]) {
+        NSLog(@"RGAppUtils Device Type: iPhone 6 Plus");
+        return DeviceType_iPhone_6_Plus;
+    }
+
+    if ([platform isEqualToString:@"iPhone7,2"]) {
+        NSLog(@"RGAppUtils Device Type: iPhone 6");
+        return DeviceType_iPhone_6;
+    }
+
+    if ([platform isEqualToString:@"iPhone8,1"]) {
+        NSLog(@"RGAppUtils Device Type: iPhone 6s");
+        return DeviceType_iPhone_6s;
+    }
+
+    if ([platform isEqualToString:@"iPhone8,2"]) {
+        NSLog(@"RGAppUtils Device Type: iPhone 6s Plus");
+        return DeviceType_iPhone_6s_Plus;
+    }
+
+    if ([platform isEqualToString:@"iPhone8,4"]) {
+        NSLog(@"RGAppUtils Device Type: iPhone SE");
+        return DeviceType_iPhone_SE;
+    }
+
+    if ([platform isEqualToString:@"iPhone9,1"]) {
+        NSLog(@"RGAppUtils Device Type: iPhone 7");
+        return DeviceType_iPhone_7;
+    }
+
+    if ([platform isEqualToString:@"iPhone9,2"]) {
+        NSLog(@"RGAppUtils Device Type: iPhone 7 Plus");
+        return DeviceType_iPhone_7_Plus;
+    }
+
+    if ([platform isEqualToString:@"iPod1,1"]) {
+        NSLog(@"RGAppUtils Device Type: iPod Touch 1G");
+        return DeviceType_iPod_Touch_1;
+    }
+
+    if ([platform isEqualToString:@"iPod2,1"]) {
+        NSLog(@"RGAppUtils Device Type: iPod Touch 2G");
+        return DeviceType_iPod_Touch_2;
+    }
+
+    if ([platform isEqualToString:@"iPod3,1"]) {
+        NSLog(@"RGAppUtils Device Type: iPod Touch 3G");
+        return DeviceType_iPod_Touch_3;
+    }
+
+    if ([platform isEqualToString:@"iPod4,1"]) {
+        NSLog(@"RGAppUtils Device Type: iPod Touch 4G");
+        return DeviceType_iPod_Touch_4;
+    }
+
+    if ([platform isEqualToString:@"iPod5,1"]) {
+        NSLog(@"RGAppUtils Device Type: iPod Touch 5G");
+        return DeviceType_iPod_Touch_5;
+    }
+
+    if ([platform isEqualToString:@"iPod7,1"]) {
+        NSLog(@"RGAppUtils Device Type: iPod Touch 6");
+        return DeviceType_iPod_Touch_6;
+    }
+
+    if ([platform isEqualToString:@"iPad1,1"]) {
+        NSLog(@"RGAppUtils Device Type: iPad 1G");
+        return DeviceType_iPad_1;
+    }
+
+    if ([platform isEqualToString:@"iPad2,1"]) {
+        NSLog(@"RGAppUtils Device Type: iPad 2");
+        return DeviceType_iPad_2;
+    }
+
+    if ([platform isEqualToString:@"iPad2,2"]) {
+        NSLog(@"RGAppUtils Device Type: iPad 2");
+        return DeviceType_iPad_2;
+    }
+
+    if ([platform isEqualToString:@"iPad2,3"]) {
+        NSLog(@"RGAppUtils Device Type: iPad 2");
+        return DeviceType_iPad_2;
+    }
+
+    if ([platform isEqualToString:@"iPad2,4"]) {
+        NSLog(@"RGAppUtils Device Type: iPad 2");
+        return DeviceType_iPad_2;
+    }
+
+    if ([platform isEqualToString:@"iPad2,5"]) {
+        NSLog(@"RGAppUtils Device Type: iPad Mini 1G");
+        return DeviceType_iPad_Mini;
+    }
+
+    if ([platform isEqualToString:@"iPad2,6"]) {
+        NSLog(@"RGAppUtils Device Type: iPad Mini 1G");
+        return DeviceType_iPad_Mini;
+    }
+
+    if ([platform isEqualToString:@"iPad2,7"]) {
+        NSLog(@"RGAppUtils Device Type: iPad Mini 1G");
+        return DeviceType_iPad_Mini;
+    }
+
+    if ([platform isEqualToString:@"iPad3,1"]) {
+        NSLog(@"RGAppUtils Device Type: iPad 3");
+        return DeviceType_iPad_3;
+    }
+
+    if ([platform isEqualToString:@"iPad3,2"]) {
+        NSLog(@"RGAppUtils Device Type: iPad 3");
+        return DeviceType_iPad_3;
+    }
+
+    if ([platform isEqualToString:@"iPad3,3"]) {
+        NSLog(@"RGAppUtils Device Type: iPad 3");
+        return DeviceType_iPad_3;
+    }
+
+    if ([platform isEqualToString:@"iPad3,4"]) {
+        NSLog(@"RGAppUtils Device Type: iPad 4");
+        return DeviceType_iPad_4;
+    }
+
+    if ([platform isEqualToString:@"iPad3,5"]) {
+        NSLog(@"RGAppUtils Device Type: iPad 4");
+        return DeviceType_iPad_4;
+    }
+
+    if ([platform isEqualToString:@"iPad3,6"]) {
+        NSLog(@"RGAppUtils Device Type: iPad 4");
+        return DeviceType_iPad_4;
+    }
+
+    if ([platform isEqualToString:@"iPad4,1"]) {
+        NSLog(@"RGAppUtils Device Type: iPad Air");
+        return DeviceType_iPad_Air;
+    }
+
+    if ([platform isEqualToString:@"iPad4,2"]) {
+        NSLog(@"RGAppUtils Device Type: iPad Air");
+        return DeviceType_iPad_Air;
+    }
+
+    if ([platform isEqualToString:@"iPad4,3"]) {
+        NSLog(@"RGAppUtils Device Type: iPad Air");
+        return DeviceType_iPad_Air;
+    }
+
+    if ([platform isEqualToString:@"iPad4,4"]) {
+        NSLog(@"RGAppUtils Device Type: iPad Mini 2G");
+        return DeviceType_iPad_Mini_2;
+    }
+
+    if ([platform isEqualToString:@"iPad4,5"]) {
+        NSLog(@"RGAppUtils Device Type: iPad Mini 2G");
+        return DeviceType_iPad_Mini_2;
+    }
+
+    if ([platform isEqualToString:@"iPad4,6"]) {
+        NSLog(@"RGAppUtils Device Type: iPad Mini 2G");
+        return DeviceType_iPad_Mini_2;
+    }
+
+    if ([platform isEqualToString:@"i386"]) {
+        NSLog(@"RGAppUtils Device Type: iPhone Simulator 32 bit");
+        return DeviceType_iPhone_Simulator_32bit;
+    }
+
+    if ([platform isEqualToString:@"x86_64"]) {
+        NSLog(@"RGAppUtils Device Type: iPhone Simulator 64 bit");
+        return DeviceType_iPhone_Simulator_64bit;
+    }
+    
+    return DeviceType_Unknown;
+}
 
 @end
