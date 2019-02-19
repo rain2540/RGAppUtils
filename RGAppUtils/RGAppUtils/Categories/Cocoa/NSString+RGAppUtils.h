1eeac27b1080df09cc326f8ba303dbf5f0189217
Smartech <rain2540@foxmail.com>
2019-02-19 08:23:26 +0800

    docs (NSString) : add comment

--- RGAppUtils/RGAppUtils/Categories/Cocoa/NSString+RGAppUtils.h
+++ RGAppUtils/RGAppUtils/Categories/Cocoa/NSString+RGAppUtils.h
@@ -36,6 +36,14 @@
 - (NSMutableAttributedString *)rg_attributedStringWithKeyword:(NSString *)keyword
                                                  keywordColor:(UIColor *)color;
 
+/**
+ 返回为 keyword 设定颜色、字体的 attributed string
+
+ @param keyword 关键词
+ @param color keyword 的颜色
+ @param font keyword 的字体
+ @return 完成 keyword 颜色、字体设定的 attributed string
+ */
 - (NSMutableAttributedString *)rg_attributedStringWithKeyword:(NSString *)keyword
                                                  keywordColor:(UIColor *)color
                                                   keywordFont:(UIFont *)font;
