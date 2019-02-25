b6d32859b28faba6cd8c023b12a431f77808e236
Smartech <rain2540@foxmail.com>
2019-02-25 08:37:05 +0800

    feat (UITextField) : add a category for UITextField

---
+++ RGAppUtils/RGAppUtils/Categories/UIKit/UITextField+RGAppUtitls.h
@@ -0,0 +1,17 @@
+//
+//  UITextField+RGAppUtitls.h
+//  RGAppUtils
+//
+//  Created by RAIN on 2019/2/25.
+//  Copyright © 2019 Smartech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UITextField (RGAppUtitls)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ RGAppUtils/RGAppUtils/Categories/UIKit/UITextField+RGAppUtitls.m
@@ -0,0 +1,13 @@
+//
+//  UITextField+RGAppUtitls.m
+//  RGAppUtils
+//
+//  Created by RAIN on 2019/2/25.
+//  Copyright © 2019 Smartech. All rights reserved.
+//
+
+#import "UITextField+RGAppUtitls.h"
+
+@implementation UITextField (RGAppUtitls)
+
+@end
