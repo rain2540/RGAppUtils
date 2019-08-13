6838f04c3147c4194ca102830fd1a8a5a587c350
Smartech <rain2540@foxmail.com>
2019-08-13 09:10:30 +0800

    feat (RGDesUtil) : add RGDesUtil

---
+++ RGAppUtils/RGAppUtils/RGBuild/RGDesUtil/RGDesUtil.h
@@ -0,0 +1,17 @@
+//
+//  RGDesUtil.h
+//  RGAppUtils
+//
+//  Created by RAIN on 2019/8/13.
+//  Copyright © 2019 Smartech. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RGDesUtil : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ RGAppUtils/RGAppUtils/RGBuild/RGDesUtil/RGDesUtil.m
@@ -0,0 +1,13 @@
+//
+//  RGDesUtil.m
+//  RGAppUtils
+//
+//  Created by RAIN on 2019/8/13.
+//  Copyright © 2019 Smartech. All rights reserved.
+//
+
+#import "RGDesUtil.h"
+
+@implementation RGDesUtil
+
+@end
