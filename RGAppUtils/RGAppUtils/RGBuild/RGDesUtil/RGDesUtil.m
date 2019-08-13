5be8b9c559fb871c3315dfe310f7420115f7fef8
Smartech <rain2540@foxmail.com>
2019-08-13 09:12:06 +0800

    feat (RGDesUtil) : add a method

--- RGAppUtils/RGAppUtils/RGBuild/RGDesUtil/RGDesUtil.m
+++ RGAppUtils/RGAppUtils/RGBuild/RGDesUtil/RGDesUtil.m
@@ -7,7 +7,44 @@
 //
 
 #import "RGDesUtil.h"
+#import <CommonCrypto/CommonCryptor.h>
 
 @implementation RGDesUtil
+
++ (NSData *)dataWithData:(NSData *)data
+                     key:(NSString *)key
+            optionForDES:(CCOperation)option
+
+{
+    char keyPtr[kCCKeySizeAES256+1];
+    bzero(keyPtr, sizeof(keyPtr));
+
+    [key getCString:keyPtr maxLength:sizeof(keyPtr) encoding:NSUTF8StringEncoding];
+
+    NSUInteger dataLength = [data length];
+
+    size_t bufferSize = dataLength + kCCBlockSizeAES128;
+    void *buffer = malloc(bufferSize);
+
+    size_t numBytesDecrypted = 0;
+    CCCryptorStatus cryptStatus = CCCrypt(option,
+                                          kCCAlgorithmDES,
+                                          kCCOptionPKCS7Padding | kCCOptionECBMode,
+                                          keyPtr,
+                                          kCCBlockSizeDES,
+                                          NULL,
+                                          [data bytes],
+                                          dataLength,
+                                          buffer,
+                                          bufferSize,
+                                          &numBytesDecrypted);
+
+    if (cryptStatus == kCCSuccess) {
+        return [NSData dataWithBytesNoCopy:buffer length:numBytesDecrypted];
+    }
+
+    free(buffer);
+    return nil;
+}
 
 @end
