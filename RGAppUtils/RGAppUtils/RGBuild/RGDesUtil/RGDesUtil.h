bd708b2efdd99b1d450a4bee44dd2d2e81ba7368
Smartech <rain2540@foxmail.com>
2019-08-16 08:09:46 +0800

    feat (RGDesUtil) : add a public method

--- RGAppUtils/RGAppUtils/RGBuild/RGDesUtil/RGDesUtil.h
+++ RGAppUtils/RGAppUtils/RGBuild/RGDesUtil/RGDesUtil.h
@@ -12,6 +12,8 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface RGDesUtil : NSObject
 
++ (NSData *)dataWithBase64EncodedString:(NSString *)string;
+
 + (NSString *)base64EncodedStringFrom:(NSData *)data;
 
 @end
--- RGAppUtils/RGAppUtils/RGBuild/RGDesUtil/RGDesUtil.m
+++ RGAppUtils/RGAppUtils/RGBuild/RGDesUtil/RGDesUtil.m
@@ -14,6 +14,70 @@ static const char encodingTable[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopq
 
 @implementation RGDesUtil
 
++ (NSData *)dataWithBase64EncodedString:(NSString *)string {
+    if (string == nil)
+        [NSException raise:NSInvalidArgumentException format:nil];
+    if ([string length] == 0)
+        return [NSData data];
+
+    static char *decodingTable = NULL;
+    if (decodingTable == NULL) {
+        decodingTable = malloc(256);
+        if (decodingTable == NULL)
+            return nil;
+        memset(decodingTable, CHAR_MAX, 256);
+        NSUInteger i;
+        for (i = 0; i < 64; i++) {
+            decodingTable[(short)encodingTable[i]] = i;
+        }
+    }
+
+    const char *characters = [string cStringUsingEncoding:NSASCIIStringEncoding];
+    if (characters == NULL)     //  Not an ASCII string!
+        return nil;
+    char *bytes = malloc((([string length] + 3) / 4) * 3);
+    if (bytes == NULL)
+        return nil;
+    NSUInteger length = 0;
+
+    NSUInteger i = 0;
+    while (YES) {
+        char buffer[4];
+        short bufferLength;
+        for (bufferLength = 0; bufferLength < 4; i++) {
+            if (characters[i] == '\0')
+                break;
+            if (isspace(characters[i]) || characters[i] == '=')
+                continue;
+            buffer[bufferLength] = decodingTable[(short)characters[i]];
+            if (buffer[bufferLength++] == CHAR_MAX) {     //  Illegal character!
+                free(bytes);
+                return nil;
+            }
+        }
+
+        if (bufferLength == 0) {
+            break;
+        }
+        if (bufferLength == 1) {  // At least two characters are needed to produce one byte!
+            free(bytes);
+            return nil;
+        }
+
+        // Decode the characters in the buffer to bytes.
+        bytes[length++] = (buffer[0] << 2) | (buffer[1] >> 4);
+        if (bufferLength > 2) {
+            bytes[length++] = (buffer[1] << 4) | (buffer[2] >> 2);
+        }
+        if (bufferLength > 3) {
+            bytes[length++] = (buffer[2] << 6) | buffer[3];
+        }
+    }
+
+    bytes = realloc(bytes, length);
+    return [NSData dataWithBytesNoCopy:bytes length:length];
+}
+
 + (NSString *)base64EncodedStringFrom:(NSData *)data {
     if ([data length] == 0)
         return @"";
