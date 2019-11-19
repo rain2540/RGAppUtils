3578be5a8c92506c11e9c61aa1e7bfaf5e18a433
Smartech <rain2540@foxmail.com>
2019-11-19 10:13:31 +0800

    docs (NSDictionary) : add a comment

--- RGAppUtils/RGAppUtils/Categories/Foundation/NSDictionary+RGAppUtils.h
+++ RGAppUtils/RGAppUtils/Categories/Foundation/NSDictionary+RGAppUtils.h
@@ -47,6 +47,8 @@
 /// @param key 待获取值的 key
 - (nullable id)rg_valueForKey:(nonnull NSString *)key;
 
+/// 获取 NSDictionary 某个 key 对应的字符串
+/// @param key 待获取字符串的 key
 - (nonnull NSString *)rg_stringForKey:(nonnull NSString *)key;
 
 @end
