2ff447e34f56a8c427e83c55b2e2b2d530a6ac6b
Smartech <rain2540@foxmail.com>
2019-11-25 08:53:57 +0800

    feat (ViewController) : add methods

--- RGAppUtils/ViewController.m
+++ RGAppUtils/ViewController.m
@@ -20,11 +20,34 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
-
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
+
+- (IBAction)actionButtonClicked:(UIButton *)sender {
+    [self attributedText];
+    [self clearString];
+}
+
+- (void)attributedText {
+    NSString *string = @"123456789";
+    UILabel *label = [[UILabel alloc] init];
+    label.frame = CGRectMake(20.0, 100.0, 300.0, 30.0);
+    label.attributedText = [string rg_attributedStringWithKeyword:@"456"
+                                                     keywordColor:[UIColor blueColor]
+                                                      keywordFont:label.font];
+    [self.view addSubview:label];
+}
+
+- (void)clearString {
+    NSString *string = @"abb";
+
+    NSLog(@"%@", [string rg_clearNilAndOtherStrings:@[]]);
+    NSLog(@"%@", [string rg_clearNilAndOtherStrings:@[@"abb", @""]]);
+    NSLog(@"%@", [string rg_clearNilAndOtherStrings:@[@"a", @"ab"]]);
+    NSLog(@"%@", [string rg_clearNilAndOtherStrings:nil]);
+}
 
 @end
