34a7d1a0650a2f4badafdf4f15484a320e9655aa
Smartech <rain2540@foxmail.com>
2020-09-04 13:20:27 +0800

    docs (UIAlertController) : update comments

--- RGAppUtils/RGAppUtils/Categories/UIKit/UIAlertController+RGAppUtils.h
+++ RGAppUtils/RGAppUtils/Categories/UIKit/UIAlertController+RGAppUtils.h
@@ -10,14 +10,11 @@
 
 @interface UIAlertController (RGAppUtils)
 
-/**
- 在视图中添加 Alert
-
- @param title Alert 标题
- @param message Alert 内容
- @param viewController 展示 Alert 的视图
- @param actions 完成的 actions
- */
+/// 在视图中显示 Alert
+/// @param title Alert 标题
+/// @param message Alert 内容
+/// @param viewController 展示 Alert 的视图
+/// @param actions 完成的 actions
 + (void)rg_showAlertWithTitle:(NSString *)title
                       message:(NSString *)message
              inViewController:(UIViewController *)viewController
@@ -27,14 +24,11 @@
                                  message:(NSString *)message
                                  actions:(NSArray<UIAlertAction *> *)actions;
 
-/**
- 在视图中添加 ActionSheet
-
- @param title ActionSheet 标题
- @param message ActionSheet 内容
- @param viewController 展示 ActionSheet 的视图
- @param actions 完成的 actions
- */
+/// 在视图中显示 ActionSheet
+/// @param title ActionSheet 标题
+/// @param message ActionSheet 内容
+/// @param viewController 展示 ActionSheet 的视图
+/// @param actions 完成的 actions
 + (void)rg_showActionSheetWithTitle:(NSString *)title
                             message:(NSString *)message
                    inViewController:(UIViewController *)viewController
