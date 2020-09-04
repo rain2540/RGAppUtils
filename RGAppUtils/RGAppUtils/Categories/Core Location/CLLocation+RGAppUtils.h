1c353e0de3921982a8a9eddeaa5187e91b0a31cd
Smartech <rain2540@foxmail.com>
2020-09-04 16:37:36 +0800

    docs (CLLocation) : update comment

--- RGAppUtils/RGAppUtils/Categories/Core Location/CLLocation+RGAppUtils.h
+++ RGAppUtils/RGAppUtils/Categories/Core Location/CLLocation+RGAppUtils.h
@@ -14,13 +14,8 @@
 /// @param baiduCoordinate 记录百度坐标经纬度的结构体
 + (CLLocationCoordinate2D)rg_locationMarsFromBaidu:(CLLocationCoordinate2D)baiduCoordinate;
 
-/**
- 火星坐标 ---> 百度坐标
-
- @param marsCoordinate 记录火星坐标经纬度的结构体
- @return 记录百度坐标经纬度的结构体
- */
+/// 火星坐标 ---> 百度坐标
+/// @param marsCoordinate 记录火星坐标经纬度的结构体
 + (CLLocationCoordinate2D)rg_locationBaiduFromMars:(CLLocationCoordinate2D)marsCoordinate;
-
 
 @end
