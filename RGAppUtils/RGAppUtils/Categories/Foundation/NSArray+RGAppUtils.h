dfef5ad910f6bea971a0fee587cea9c5e2a60723
Smartech <rain2540@foxmail.com>
2020-10-16 16:52:10 +0800

    chore (project) : add category of NSArray

---
+++ RGAppUtils/RGAppUtils/Categories/Foundation/NSArray+RGAppUtils.h
@@ -0,0 +1,17 @@
+//
+//  NSArray+RGAppUtils.h
+//  RGAppUtils
+//
+//  Created by RAIN on 2020/10/16.
+//  Copyright © 2020 Smartech. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSArray (RGAppUtils)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ RGAppUtils/RGAppUtils/Categories/Foundation/NSArray+RGAppUtils.m
@@ -0,0 +1,13 @@
+//
+//  NSArray+RGAppUtils.m
+//  RGAppUtils
+//
+//  Created by RAIN on 2020/10/16.
+//  Copyright © 2020 Smartech. All rights reserved.
+//
+
+#import "NSArray+RGAppUtils.h"
+
+@implementation NSArray (RGAppUtils)
+
+@end
